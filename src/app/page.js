b8798175514b946dc54216d61ef5a458c7c7f336
Dilip <dilip.s.hiremath@gmail.com>
2024-05-25 16:15:43 +0530

    intro page

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,19 @@
-// `app/page.js` is the UI for the `/` URL
+// `app/page.js` is the UI for the `/` URLimport { Suspense } from "react";
+import { AppHeader, AppFooter, AppMetadata } from "../components";
+import Loading from "./loading";
+import "../styles/globals.css";
+import { ThemeContext } from "../context";
+// "use client";
+
+import { WelcomeSection } from "./sections";
+
 export default function Page() {
-  return <h1>Hello, Home page!</h1>
+	return (
+		<div className="container-md">
+			<WelcomeSection />
+			{/* <AboutSection />
+			<ProjectsSection />
+			<TechnologiesSection /> */}
+		</div>
+	);
 }
---
+++ src/utils/index.js
@@ -0,0 +1,3 @@
+export * from "./motions.js";
+export * from "./media_query.js";
+export * from "./theme-config";
