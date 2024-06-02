f4c220914c9db2daaf8eb2a1511d78aeaa63ea65
Dilip <dilip.s.hiremath@gmail.com>
2024-06-02 21:54:21 +0530

    added projects and skills section

--- src/app/page.js
+++ src/app/page.js
@@ -1,19 +1,19 @@
+"use client";
 // `app/page.js` is the UI for the `/` URLimport { Suspense } from "react";
 import { AppHeader, AppFooter, AppMetadata } from "../components";
 import Loading from "./loading";
 import "../styles/globals.css";
 import { ThemeContext } from "../context";
-// "use client";
 
-import { WelcomeSection } from "./sections";
+import { WelcomeSection, AboutSection, ProjectsSection, TechnologiesSection } from "./sections";
 
 export default function Page() {
 	return (
 		<div className="container-md">
 			<WelcomeSection />
-			{/* <AboutSection />
-			<ProjectsSection />
-			<TechnologiesSection /> */}
+			<AboutSection />
+			{/* <ProjectsSection /> */}
+			<TechnologiesSection />
 		</div>
 	);
 }
--- src/constants/technologies.js
+++ src/constants/technologies.js
@@ -14,46 +14,48 @@ import MuiIcon from "../public/assets/svg/mui.svg";
 import ChakraIcon from "../public/assets/svg/chakraui.svg";
 import StyledIcon from "../public/assets/svg/styledcomponents.svg";
 
+const flutter = () => <RiFlutterFill size={32} /> 
+
 export const TECHNOLOGIES = [
 	{
 		category: "Mobile",
-		items: [{ name: "Flutter", icon: <RiFlutterFill size={32} /> }]
-	},
-	{
-		category: "Front-end",
-		items: [
-			{ name: "HTML", icon: <AiFillHtml5 size={32} /> },
-			{ name: "CSS", icon: <DiCss3 size={32} /> },
-			{ name: "JS", icon: <IoLogoJavascript size={32} /> },
-			{ name: "React", icon: <FaReact size={32} /> },
-			{ name: "Next", icon: <TbBrandNextjs size={32} /> },
-			{ name: "Tailwind CSS", icon: <TbBrandTailwind size={32} /> },
-			{ name: "MUI", icon: <MuiIcon width={32} /> },
-			{ name: "AntD", icon: <AiOutlineAntDesign size={32} /> },
-			{ name: "Bootstrap", icon: <BsBootstrap size={32} /> },
-			{ name: "Chakra", icon: <ChakraIcon width={32} /> },
-			{ name: "styled components", icon: <StyledIcon width={32} /> },
-			{ name: "HeadlessUI", icon: <HeadlessUiIcon width={32} /> }
-		]
-	},
-	{
-		category: "UI tools",
-		items: [
-			{ name: "Figma", icon: <FaFigma size={32} /> },
-			{ name: "Zeplin", icon: <ZeplinIcon width={36} /> },
-			{ name: "XD", icon: <AdobeXDIcon width={32} /> }
-		]
-	},
-	{
-		category: "Other tools",
-		items: [
-			{ name: "WordPress CMS", icon: <FaWordpressSimple size={32} /> },
-			{ name: "Jira", icon: <JiraIcon width={32} /> },
-			{ name: "Trello", icon: <FaTrello size={32} /> },
-			{ name: "Github", icon: <AiFillGithub size={32} /> },
-			{ name: "Gitlab", icon: <AiFillGitlab size={32} /> },
-			{ name: "VsCode", icon: <DiVisualstudio size={32} /> },
-			{ name: "WebStorm", icon: <WebstormIcon width={32} /> }
-		]
+		items: [{ name: "Flutter", icon: flutter()}]
 	}
+	// {
+	// 	category: "Front-end",
+	// 	items: [
+	// 		{ name: "HTML", icon: <AiFillHtml5 size={32} /> },
+	// 		{ name: "CSS", icon: <DiCss3 size={32} /> },
+	// 		{ name: "JS", icon: <IoLogoJavascript size={32} /> },
+	// 		{ name: "React", icon: <FaReact size={32} /> },
+	// 		{ name: "Next", icon: <TbBrandNextjs size={32} /> },
+	// 		{ name: "Tailwind CSS", icon: <TbBrandTailwind size={32} /> },
+	// 		{ name: "MUI", icon: <MuiIcon width={32} /> },
+	// 		{ name: "AntD", icon: <AiOutlineAntDesign size={32} /> },
+	// 		{ name: "Bootstrap", icon: <BsBootstrap size={32} /> },
+	// 		{ name: "Chakra", icon: <ChakraIcon width={32} /> },
+	// 		{ name: "styled components", icon: <StyledIcon width={32} /> },
+	// 		{ name: "HeadlessUI", icon: <HeadlessUiIcon width={32} /> }
+	// 	]
+	// }
+	// {
+	// 	category: "UI tools",
+	// 	items: [
+	// 		{ name: "Figma", icon: <FaFigma size={32} /> },
+	// 		{ name: "Zeplin", icon: <ZeplinIcon width={36} /> },
+	// 		{ name: "XD", icon: <AdobeXDIcon width={32} /> }
+	// 	]
+	// },
+	// {
+	// 	category: "Other tools",
+	// 	items: [
+	// 		{ name: "WordPress CMS", icon: <FaWordpressSimple size={32} /> },
+	// 		{ name: "Jira", icon: <JiraIcon width={32} /> },
+	// 		{ name: "Trello", icon: <FaTrello size={32} /> },
+	// 		{ name: "Github", icon: <AiFillGithub size={32} /> },
+	// 		{ name: "Gitlab", icon: <AiFillGitlab size={32} /> },
+	// 		{ name: "VsCode", icon: <DiVisualstudio size={32} /> },
+	// 		{ name: "WebStorm", icon: <WebstormIcon width={32} /> }
+	// 	]
+	// }
 ];
